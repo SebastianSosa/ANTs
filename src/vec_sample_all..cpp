578783af9d94f38e3d9d964b668cb5c99a9220ae
SebastianSosa <s.sosa@live.fr>
2018-09-27 11:27:42 +0800

    genereic function to sample an entire vectore

---
+++ src/vec_sample_all..cpp
@@ -0,0 +1,89 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+namespace impl {
+
+template <int RTYPE>
+Vector<RTYPE> sample(const Vector<RTYPE>& vec)
+{
+  Vector<RTYPE> res=Rcpp::sample(vec,vec.size(), FALSE);
+
+  return res;
+}
+
+template <int RTYPE>
+IntegerVector as_factor(const Vector<RTYPE>& x ) {
+  Vector<RTYPE> levs = sort_unique(x);
+  IntegerVector out = match(x, levs);
+  out.attr("levels") = as<CharacterVector>(levs);
+  out.attr("class") = "factor";
+  return out;
+}
+
+} 
+
+// [[Rcpp::export]]
+SEXP vec_sample_all(SEXP vec) {
+  switch (TYPEOF(vec)) {
+  case INTSXP: {
+    if(Rf_isFactor(vec)){
+      IntegerVector v1=vec;
+      bool test1=Rf_isNull(v1.attr("class"));
+      if(test1==FALSE){
+        std::string r1=v1.attr("class");
+        if(r1=="factor"){
+          CharacterVector vec1=vec;
+          vec=Rcpp::sample(vec1,vec1.size(),false);
+          CharacterVector levs = sort_unique(vec1);
+          IntegerVector result = match(vec1, levs);
+          result.attr("levels") = levs;
+          result.attr("class") = "factor";
+          return result;
+        }
+        else{
+          SEXP res =impl::sample(as<IntegerVector>(vec));
+          res=impl::as_factor(as<IntegerVector>(res));
+          return res;
+        }
+      }
+    }
+    else{
+      return impl::sample(as<IntegerVector>(vec));
+    }
+    
+  }
+  case REALSXP: {
+    if(Rf_isFactor(vec)){
+      SEXP res =impl::sample(as<NumericVector>(vec));
+      res=impl::as_factor(as<NumericVector>(res));
+      return res;
+    }
+    else{
+      return impl::sample(as<NumericVector>(vec));
+    }
+  }
+  case STRSXP: {
+    if(Rf_isFactor(vec)){
+      SEXP res =impl::sample(as<CharacterVector>(vec));
+      res=impl::as_factor(as<CharacterVector>(res));
+      return res;
+    }
+    else{
+      return impl::sample(as<CharacterVector>(vec));
+    }
+  }
+  case LGLSXP: {
+    return impl::sample(as<LogicalVector>(vec));
+  }
+  case CPLXSXP: {
+    return impl::sample(as<ComplexVector>(vec));
+  }
+  default: {
+    warning(
+      "Invalid SEXPTYPE %d (%s).\n",
+      TYPEOF(vec), type2name(vec)
+    );
+    return R_NilValue;
+  }
+  }
+}
