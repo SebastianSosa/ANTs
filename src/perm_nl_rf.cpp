434e73f91283b015091b3574e16c226bc1fc6fa7
SebastianSosa <s.sosa@live.fr>
2018-09-27 12:50:30 +0800

    perm_net_nl_rf modifications
    debuging perm_net_nl_rf function and increasing computation speed

--- src/perm_nl_rf.cpp
+++ src/perm_nl_rf.cpp
@@ -14,53 +14,91 @@
 
 #include <Rcpp.h>
 using namespace Rcpp;
+SEXP vec_merge(SEXP vec1, SEXP vec2);
 DataFrame ldf_merge(List ldf);
-SEXP vec_sample_all(SEXP vec1);
+SEXP vec_sample_all(SEXP vec);
 //' @title Node lable permutation with random factors
 //' @description Perorm node label permutation on list of data frames
 //' @keywords internal
 // [[Rcpp::export]]
 List perm_nl_rf(List ldf, NumericVector lables, int nperm, bool progress) {
+  // Object storing the result
   List pldf(nperm+1);
-  pldf[0]=ldf_merge(ldf);
+  // First data frame is se fusion of all data frames in the list
+  DataFrame df= ldf_merge(ldf);
+  pldf[0]=clone(df);
+  
+  // Creating a list of list of vector for each label declare by user
+  int n_df=ldf.size();
+  List lable_to_permut(n_df);
+  int n=0;
+
   if(progress==TRUE){
+    // For each permutations declared
     for(int a=1; a<nperm+1;a++){
-      List ldf2=clone(ldf);
       std::cout<<"\r"<<"permutation: "<<a;
-      for(int b=0;b<ldf.size();b++){
-        DataFrame d=ldf2[b];
-        DataFrame df=clone(d);
-        for(int c=0;c<lables.size();c++){
-          int n=lables[c]-1;
-          SEXP vec=df[n];
+      //For each lables declared
+      for(int b=0;b<lables.size();b++){
+        //Extracting first element
+        DataFrame d1=ldf[0];
+        // C++ count
+        n=lables[b]-1;
+        // Extract the column
+        SEXP vec_all=d1[n];
+        vec_all=vec_sample_all(vec_all);
+
+        // For each elements of the list
+        for(int c=1;c<ldf.size();c++){
+          // Extract data frame
+          DataFrame d=ldf[c];
+          // Extract the column
+          SEXP vec=d[n];
+          // Sample it
           SEXP newvec=vec_sample_all(vec);
-          df[n]=newvec;
+          // Merge it with previous labels vector in previous data frames
+          vec_all=vec_merge(vec_all, newvec);
         }
-        ldf2[b]=df;
+        
+        df[n]=vec_all;
+
       }
-      DataFrame tmp=ldf_merge(ldf2);
-      tmp.attr("permutation") = a;
-      pldf[a]=tmp;
+
+      df.attr("permutation") = a;
+      pldf[a]=clone(df);
     }
     std::cout<<"\n"<<std::endl;
   }
   else{
+    // For each permutations declared
     for(int a=1; a<nperm+1;a++){
-      List ldf2=clone(ldf);
-      for(int b=0;b<ldf.size();b++){
-        DataFrame d=ldf2[b];
-        DataFrame df=clone(d);
-        for(int c=0;c<lables.size();c++){
-          int n=lables[c]-1;
-          SEXP vec=df[n];
+
+      for(int b=0;b<lables.size();b++){
+        //Extracting first element
+        DataFrame d1=ldf[0];
+        // C++ count
+        n=lables[b]-1;
+        // Extract the column
+        SEXP vec_all=d1[n];
+        vec_all=vec_sample_all(vec_all);
+        
+        // For each elements of the list
+        for(int c=1;c<ldf.size();c++){
+          // Extract data frame
+          DataFrame d=ldf[c];
+          // Extract the column
+          SEXP vec=d[n];
+          // Sample it
           SEXP newvec=vec_sample_all(vec);
-          df[n]=newvec;
+          // Merge it with previous labels vector in previous data frames
+          vec_all=vec_merge(vec_all, newvec);
         }
-        ldf2[b]=df;
+        
+        df[n]=vec_all;
+        
       }
-      DataFrame tmp=ldf_merge(ldf2);
-      tmp.attr("permutation") = a;
-      pldf[a]=tmp;
+      
+      df.attr("permutation") = a;
+      pldf[a]=clone(df);
     }
   }
   return pldf;
