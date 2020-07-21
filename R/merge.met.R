37a70bfcb1b4c3a0f2595a2071a0a8de2b5d1625
sosa <s.sosa@live.fr>
2020-07-21 18:33:50 +0200

    function to merge metrics

--- R/merge.met.R
+++ R/merge.met.R
@@ -1,5 +1,11 @@
 #' @title Merge data frame with metric
 #' @description merge metric to a data frame
+#' @param vec a vector to merge to argument df
+#' @param names a string vector of same length as argument vec, with IDs of each element of argument vec
+#' @param df a data frame to merge vec
+#' @param dfid an integer indicating the column in argument df in which IDS are stored.
+#' @param met a string for the new column merged in argument df
+#' @keywords internal
 merge.met <- function(vec, names, df, dfid, met){
   ori.name = colnames(df)[dfid]
   d2 = data.frame(names, vec)
